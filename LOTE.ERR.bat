c0bf3bb815cdaa51aa46fb4eb703d0287dd1cb88
JhordanRojas <56855169+jhordanrojas@users.noreply.github.com>
2019-11-24 15:31:13 -0500

    se agrego el archivo por lote

---
+++ LOTE.ERR.bat
@@ -0,0 +1,51 @@
+python Rojas/While/ejercicio01.py 2056
+python Rojas/While/ejercicio02.py 54.65
+python Rojas/While/ejercicio03.py Mat654atica
+python Rojas/While/ejercicio04.py 165.5
+python Rojas/While/ejercicio05.py 10.55
+
+python Rojas/For/ejercicio01.py 0 as
+python Rojas/For/ejercicio02.py 1 65.56
+python Rojas/For/ejercicio03.py 0.2 45
+python Rojas/For/ejercicio04.py 1.4 4
+python Rojas/For/ejercicio05.py 2.3 1.20
+
+python Rojas/Iteration/ejercicio01.py xasdzw
+python Rojas/Iteration/ejercicio02.py adsfe
+python Rojas/Iteration/ejercicio03.py QPSDH
+python Rojas/Iteration/ejercicio04.py 4S65
+python Rojas/Iteration/ejercicio05.py jetdf
+
+python Rojas/Range/ejercicio01.py 0 .2
+python Rojas/Range/ejercicio02.py 1.50 1df
+python Rojas/Range/ejercicio03.py 2 132d
+python Rojas/Range/ejercicio04.py 1.252
+python Rojas/Range/ejercicio05.py 2.455
+
+
+
+
+python Linares/While/ejercicio01.py 3.4 5.8 2
+python Linares/While/ejercicio02.py 201.3 10
+python Linares/While/ejercicio03.py 21.4 5
+python Linares/While/ejercicio04.py 1000 9.5
+python Linares/While/ejercicio05.py 3.14 13.2 5
+
+python Linares/For/ejercicio01.py -100
+python Linares/For/ejercicio02.py 40 60
+python Linares/For/ejercicio03.py 20 -100
+python Linares/For/ejercicio04.py 30 -30
+python Linares/For/ejercicio05.py 100 20
+
+python Linares/Range/ejercicio01.py 101.5
+python Linares/Range/ejercicio02.py 200.98
+python Linares/Range/ejercicio03.py 98.5 50
+python Linares/Range/ejercicio04.py 90.60
+python Linares/Range/ejercicio05.py 135.98
+
+python Linares/Iteration/ejercicio01.py abcd
+python Linares/Iteration/ejercicio02.py 123456
+python Linares/Iteration/ejercicio03.py 1234
+python Linares/Iteration/ejercicio04.py abcd
+python Linares/Iteration/ejercicio05.py ABCD
+
---
+++ LOTE.bat
@@ -0,0 +1,51 @@
+python Rojas/While/ejercicio01.py 2019
+python Rojas/While/ejercicio02.py 524
+python Rojas/While/ejercicio03.py Matematica
+python Rojas/While/ejercicio04.py 18
+python Rojas/While/ejercicio05.py 10
+
+python Rojas/For/ejercicio01.py 0 50
+python Rojas/For/ejercicio02.py 1 9
+python Rojas/For/ejercicio03.py 1 22
+python Rojas/For/ejercicio04.py 1 4
+python Rojas/For/ejercicio05.py 1 10
+
+python Rojas/Iteration/ejercicio01.py xyzw
+python Rojas/Iteration/ejercicio02.py acdbe
+python Rojas/Iteration/ejercicio03.py QPNM
+python Rojas/Iteration/ejercicio04.py 4213
+python Rojas/Iteration/ejercicio05.py jhgf
+
+python Rojas/Range/ejercicio01.py 0 10
+python Rojas/Range/ejercicio02.py 100 15
+python Rojas/Range/ejercicio03.py 2 16
+python Rojas/Range/ejercicio04.py 10
+python Rojas/Range/ejercicio05.py 21
+
+
+
+
+
+python Linares/While/ejercicio01.py 5 1 5
+python Linares/While/ejercicio02.py 100 5
+python Linares/While/ejercicio03.py 20 10
+python Linares/While/ejercicio04.py 1000 9
+python Linares/While/ejercicio05.py 3.14 5 6
+
+python Linares/For/ejercicio01.py 500
+python Linares/For/ejercicio02.py 55 125
+python Linares/For/ejercicio03.py 30 90
+python Linares/For/ejercicio04.py -30 30
+python Linares/For/ejercicio05.py 200 1350
+
+python Linares/Range/ejercicio01.py 100
+python Linares/Range/ejercicio02.py 200
+python Linares/Range/ejercicio03.py 100 200
+python Linares/Range/ejercicio04.py 100
+python Linares/Range/ejercicio05.py 135
+
+python Linares/Iteration/ejercicio01.py 1234
+python Linares/Iteration/ejercicio02.py ABFRMPQS
+python Linares/Iteration/ejercicio03.py ABFR
+python Linares/Iteration/ejercicio04.py 9876
+python Linares/Iteration/ejercicio05.py CPSH
