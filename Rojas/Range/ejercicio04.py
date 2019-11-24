cf5b00e2eef4889a883e7f6f88b61726ceec4852
JhordanRojas <56855169+jhordanrojas@users.noreply.github.com>
2019-11-24 15:46:07 -0500

    se agrego el cuarto ejercicio de bucle range

---
+++ Rojas/Range/ejercicio04.py
@@ -0,0 +1,10 @@
+import os
+# Imprimir los "Z" primeros numeros y elevarlos al cubo
+z=int(os.sys.argv[1])
+a=0
+#iterador
+for a in range(z):
+    print("el numero es:",a ,"y su cubo es:",(a**3))
+
+#Fin_iterador_rango
+print("fin del bucle")
