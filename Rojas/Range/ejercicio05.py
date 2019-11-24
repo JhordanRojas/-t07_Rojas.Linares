61aef5179cb656d7c0822aa311b4b917d344c6f3
JhordanRojas <56855169+jhordanrojas@users.noreply.github.com>
2019-11-24 15:46:30 -0500

    se agrego el quinto ejercicio de bucle range

---
+++ Rojas/Range/ejercicio05.py
@@ -0,0 +1,9 @@
+import os
+# Imprimir los 20 primeros numeros naturales divididos entre 10
+n=int(os.sys.argv[1])
+#iterador
+for m in range(n):
+    print("el numero es",m ,"y dividivo entre 10 es:",(m)/10)
+
+#Fin_iterador_rango
+print("fin del bucle")
