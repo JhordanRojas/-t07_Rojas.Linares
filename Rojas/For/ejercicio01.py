fc5c36513e5f001de2bb8adf0248a20579030bc3
JhordanRojas <56855169+jhordanrojas@users.noreply.github.com>
2019-11-24 15:33:19 -0500

    se agrego el primer ejercicio de bucle <<para>>

---
+++ Rojas/For/ejercicio01.py
@@ -0,0 +1,13 @@
+import os
+#sumar los primeros 50 numeros naturales
+a=int(os.sys.argv[1])
+b=int(os.sys.argv[2])
+summ=0
+while (a<=b):
+    summ += a
+    a+=1
+
+#Fin_para
+
+print("La suma de los primeros 50 numeros es:", summ)
+print("fin del bucle")
